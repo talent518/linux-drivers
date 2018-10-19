332a498699de9accd75379fe848bad52f3b1dbb3
ZhangBaoCai <talent518@yeah.net>
2018-10-20 01:49:04 +0800

    hello: 解决时区问题．

--- hello/hello.c
+++ hello/hello.c
@@ -8,6 +8,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include <linux/string.h>
 #include <linux/time.h>
 #include <linux/timer.h>
 #include <linux/timex.h>
@@ -16,6 +17,7 @@
 MODULE_LICENSE("Dual BSD/GPL");   //内核2.6以上版本建议我们加上这个license
 
 struct timer_list mytimer;
+int __timezone = 0;
 
 void mytimer_function(unsigned long arg) {
 	struct timex txc;
@@ -24,10 +26,20 @@ void mytimer_function(unsigned long arg) {
 	mod_timer(&mytimer, jiffies + HZ);
 
 	do_gettimeofday(&(txc.time));
-	txc.time.tv_sec -= sys_tz.tz_minuteswest * 60; // 把UTC时间调整为本地时间
+
+#ifdef TIMEZONE
+	if(__timezone) {
+		txc.time.tv_sec += __timezone;
+	} else {
+#endif
+		txc.time.tv_sec -= sys_tz.tz_minuteswest * 60; // 把UTC时间调整为本地时间
+#ifdef TIMEZONE
+	}
+#endif
+	
 	rtc_time_to_tm(txc.time.tv_sec, &tm);
 
-	printk("%d-%02d-%02d %02d:%02d:%02d: mytimer function (%d)...\n", tm.tm_year+1900, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, arg);
+	printk("%04d-%02d-%02d %02d:%02d:%02d: mytimer function (%lu)...\n", tm.tm_year+1900, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, arg);
 }
 
 static int __init hello_init(void) {
@@ -40,6 +52,22 @@ static int __init hello_init(void) {
 	mytimer.function = mytimer_function; // 设置定时器超时函数
 	
 	add_timer(&mytimer);
+
+#ifdef TIMEZONE
+{
+	char sign='+';
+	int hour=0, minute=0;
+	if(sscanf(TIMEZONE, "%c%d:%d", &sign, &hour, &minute)!=3) {
+		printk(KERN_ERR "sscanf input items error for TIMEZONE micro\n");
+	}
+	__timezone = hour*3600+minute*60;
+	if(sign == '-') {
+		__timezone = - __timezone;
+	}
+	printk("timezone = "TIMEZONE" = %d, sign = %c, hour = %d, minute = %d\n", __timezone, sign, hour, minute);
+}
+#endif
+
 
 	return 0;
 }
